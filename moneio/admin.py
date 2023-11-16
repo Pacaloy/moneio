abb48bb3b15bf930bdc50f28c7c35c27c7249565
Carlo <cruz.carlo.diaz@gmail.com>
2023-11-16 23:17:12 +0800

    Add Account and MoneyInOut models

--- moneio/admin.py
+++ moneio/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Account, MoneyInOut
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Account)
+admin.site.register(MoneyInOut)
--- moneio/models.py
+++ moneio/models.py
@@ -7,4 +7,24 @@ class User(AbstractUser):
 
   def __str__(self):
     return f"{self.id}: {self.username}"
+
+
+class Account(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="account_owner")
+  name = models.CharField(max_length=255)
+  initial_balance = models.DecimalField(max_digits=12, decimal_places=2)
+  initial_balance_date = models.DateField()
+
+  def __str__(self):
+    return f"{self.user.username} -> {self.name}: {self.initial_balance}"
   
+
+class MoneyInOut(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="money_in_out_owner")
+  name = models.CharField(max_length=255)
+  price = models.DecimalField(max_digits=12, decimal_places=2)
+  date = models.DateField()
+  is_expense = models.BooleanField()
+
+  def __str__(self):
+    return f"{self.user.username} -> {self.name}: {self.price}"
