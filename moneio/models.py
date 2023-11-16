c910e3a529d2a2efb73c9b6ea8e6d8087e00eda6
Carlo <cruz.carlo.diaz@gmail.com>
2023-11-17 00:26:42 +0800

    Add account field in MoneyInOut model

--- moneio/models.py
+++ moneio/models.py
@@ -10,21 +10,22 @@ class User(AbstractUser):
 
 
 class Account(models.Model):
-  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="account_owner")
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="account_user")
   name = models.CharField(max_length=255)
   initial_balance = models.DecimalField(max_digits=12, decimal_places=2)
   initial_balance_date = models.DateField()
 
   def __str__(self):
     return f"{self.user.username} -> {self.name}: {self.initial_balance}"
-  
+
 
 class MoneyInOut(models.Model):
-  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="money_in_out_owner")
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="money_in_out_user")
   name = models.CharField(max_length=255)
   price = models.DecimalField(max_digits=12, decimal_places=2)
   date = models.DateField()
   is_expense = models.BooleanField()
+  account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name="money_in_out_account")
 
   def __str__(self):
-    return f"{self.user.username} -> {self.name}: {self.price}"
+    return f"{self.user.username} -> {self.account.name} -> {self.name}: {self.price}"
