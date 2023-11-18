5d6617a220f5e6168ec7b477af6866fe036bbc98
Carlo <cruz.carlo.diaz@gmail.com>
2023-11-19 00:16:36 +0800

    add default view and moneio view

--- moneio/static/moneio/index.js
+++ moneio/static/moneio/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('#moneyInBtn').onclick = () => console.log('money in');
-  document.querySelector('#moneyOutBtn').onclick = () => console.log('money out');
+  document.querySelector('#moneyInBtn').onclick = moneio;
+  document.querySelector('#moneyOutBtn').onclick = moneio;
 
   // Use buttons to toggle between views
   document.querySelector('#dashboardBtn').onclick = loadDashboard;
@@ -15,7 +15,8 @@ function loadDashboard() {
   // View dashboard and hide breakdown
   document.querySelector('#dashboardView').style.display = 'block';
   document.querySelector('#breakdownView').style.display = 'none';
-  
+  document.querySelector('#moneioView').style.display = 'none';
+
   document.querySelector('#addAccountBtn').onclick = () => console.log('add account');
   document.querySelector('#addFloatingBtn').onclick = () => console.log('add floating');
 }
@@ -25,4 +26,17 @@ function loadBreakdown() {
   // View breakdown and hide dashboard
   document.querySelector('#dashboardView').style.display = 'none';
   document.querySelector('#breakdownView').style.display = 'block';
+}
+
+function moneio() {
+  
+  // View moneio view and hide default view
+  document.querySelector('#defaultView').style.display = 'none';
+  document.querySelector('#moneioView').style.display = 'block';
+
+  // Close button for to return to default view 
+  document.querySelector('#closeMoneioViewBtn').onclick = () => {
+    document.querySelector('#defaultView').style.display = 'block';
+    document.querySelector('#moneioView').style.display = 'none';
+  };
 }
